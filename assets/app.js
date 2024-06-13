304ae70339e4deacd1c767fdad48834acb031633
Ladhari Sonia <soniamessabis@hotmail.fr>
2024-06-13 14:24:28 +0200

    css

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,10 @@ import './bootstrap.js';
 import './styles/scss/main.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+
+// Import necessary images
+function importAll(r) {
+    return r.keys().map(r);
+  }
+  
+  importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
